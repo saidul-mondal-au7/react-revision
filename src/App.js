3eb7a74d064c922edbbdc2ff05ef18e9e2fc8e1b
Saidul Mondal <mondalsaidul3232@gmail.com>
2020-12-20 15:30:37 +0530

    react-revision

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons : [
+      { "name": "Saidul", "age": 32 },
+      { "name": "Sukhia", "age": 27 },
+      { "name": "Ishrat", "age": 4 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked!')
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>This is Rivision-REACT</h1>
+        <h2>let's crack the interview</h2>
+        <button onClick={this.switchNameHandler}>switch</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > Hobby: Cooking</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
