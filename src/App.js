33698c34bc6d5f2c6ac67bb8f8614040ff7aa60b
Saidul Mondal <mondalsaidul3232@gmail.com>
2020-12-20 17:40:47 +0530

    class-func

--- src/App.js
+++ src/App.js
@@ -1,34 +1,85 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
-  state = {
-    persons : [
+const App = (props) => {
+  const [setPerson, personHandler] = useState(
+    {
+      persons : [
+        { "name": "Saidul", "age": 32 },
+        { "name": "Sukhia", "age": 27 },
+        { "name": "Ishrat", "age": 4 }
+      ]
+    }
+  )
+
+  const switchNameHandler = () => {
+    // console.log('was clicked!')
+    personHandler({
+      persons: [
       { "name": "Saidul", "age": 32 },
-      { "name": "Sukhia", "age": 27 },
-      { "name": "Ishrat", "age": 4 }
-    ]
+      { "name": "Sikha", "age": 26 },
+      { "name": "Ishu", "age": 3 }
+      ]
+    })
   }
 
-  switchNameHandler = () => {
-    console.log('was clicked!')
-  }
-
-  render(){
     return (
       <div className="App">
         <h1>This is Rivision-REACT</h1>
         <h2>let's crack the interview</h2>
-        <button onClick={this.switchNameHandler}>switch</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > Hobby: Cooking</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={switchNameHandler}>Switch</button>
+        <Person name={setPerson.persons[0].name} age={setPerson.persons[0].age} />
+        <Person name={setPerson.persons[1].name} age={setPerson.persons[1].age} > Hobby: Cooking</Person>
+        <Person name={setPerson.persons[2].name} age={setPerson.persons[2].age} />
       </div>
     );
-  }
-  
 }
+  
 
 export default App;
+
+
+// import React, { Component } from 'react';
+
+// import Person from './Person/Person';
+// import './App.css';
+
+// class App extends Component {
+//   state = {
+//     persons : [
+//       { "name": "Saidul", "age": 32 },
+//       { "name": "Sukhia", "age": 27 },
+//       { "name": "Ishrat", "age": 4 }
+//     ]
+//   }
+
+//   switchNameHandler = () => {
+//     // console.log('was clicked!')
+//     this.setState({
+//       persons: [
+//       { "name": "Saidul", "age": 32 },
+//       { "name": "Sikha", "age": 26 },
+//       { "name": "Ishu", "age": 3 }
+//       ]
+//     })
+//   }
+
+//   render(){
+//     return (
+//       <div className="App">
+//         <h1>This is Rivision-REACT</h1>
+//         <h2>let's crack the interview</h2>
+//         <button onClick={this.switchNameHandler}>Switch</button>
+//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > Hobby: Cooking</Person>
+//         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+//       </div>
+//     );
+//   }
+  
+// }
+
+// export default App;
+
